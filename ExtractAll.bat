16448717739c8fc06fcf39389311940fdb4c1459
alvinan <anay@cn.ibm.com>
2016-07-07 13:08:32 +0800

    xxx
    xxx

--- ExtractAll.bat
+++ ExtractAll.bat
@@ -1,15 +1,3 @@
-<<<<<<< HEAD
 ren *.tpkg *.zip
 "C:\Program Files\7-Zip\7z.exe" -y x *.zip -o*
 ren *.zip *.tpkg
-
-
-add test
-tesvvvxxxxtxxx
-=======
-ren *.tpkg *.zip
-"C:\Program Files\7-Zip\7z.exe" -y x *.zip -o*
-ren *.zip *.tpkg
->>>>>>> origin/master
-
-fjiejwofjewoifjew
